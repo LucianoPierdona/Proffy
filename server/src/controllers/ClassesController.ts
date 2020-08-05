f61f21f0f3dca481bd53f4f24489a0a99edc4cb7
Luciano Pierdona <lucianompj1@gmail.com>
2020-08-05 13:02:44 -0300

    Day 2 Update

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -1,4 +1,4 @@
-import { Request, Response} from 'express'
+import { Request, Response, response} from 'express'
 
 import db from '../database/connection';
 import convertHourToMinutes from '../utils/convertHourToMinutes';
@@ -10,7 +10,39 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
-    async create(req :Request, res: Response) {
+    async index(req: Request, res: Response) {
+        const filters = req.query;
+
+        const subject = filters.subject as string;
+        const week_day = filters.week_day as string;
+        const time = filters.time as string;
+
+        if (!filters.subject || !filters.week_day || !filters.time) {
+            return res.status(400).json({
+                error: 'Missing filters to search classes'
+            });
+        }
+
+        const timeInMinutes = convertHourToMinutes(time);
+    
+        const classes = await db('classes')
+            .whereExists(function () {
+                this.select('class_schedule.*')
+                    .from('class_schedule')
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                    .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+                })
+            .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users*']);
+
+        return response.send();
+    
+    }
+    
+    async create(req: Request, res: Response) {
         const {
             name,
             avatar,
@@ -56,6 +88,7 @@ export default class ClassesController {
         
             return res.status(201).send();
         } catch (err) {
+            console.log(err);
             await trx.rollback();
             return res.status(400).json({
                 error: 'Unexpected error while creating new class'
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 const app = express();
 
 // GET: Buscar ou listar uma informação
 // POST: Criar alguma nova informação
 // PUT: Atualizar uma informação existente
 // DELETE: Deletar uma informação existente
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
